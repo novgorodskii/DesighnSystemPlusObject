5c1e84d0b2524289fce2c2c9d76cec1bc7fee069
novgorodskii <novgorodskiji@gmail.com>
2019-12-03 18:04:59 +0200

    add left nav bar

--- src/components/NavList/NavList.jsx
+++ src/components/NavList/NavList.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 import './NavList.sass';
+import data from '../../data';
+
+const NavListItem = ({name, arr}) => {
+  return (
+    <div className="nav-list-item">
+      <div className="nav-list-title">{name}</div>
+      {
+        arr.map(item => {
+          return (
+            <div key={item.id} className="nav-list-item-name">
+              {item.name}
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+};
 
 const NavList = () => {
+  const list = data.map(item => {
+    return <NavListItem key={item.id}  name={item.name} arr={item.items}/>
+  });
   return (
-    <div className="nav-list">NavList</div>
+    <div className="nav-list">
+      {list}
+    </div>
   );
 };
 
---
+++ src/data.js
@@ -0,0 +1,60 @@
+const data = [
+  {
+    name: "Типографика",
+    id: 1,
+    items: [
+      {
+        name: "Heading",
+        id: 1,
+      },
+      {
+        name: "Paragraph",
+        id: 2,
+      }
+    ]
+  },
+  {
+    name: "Кнопки и ссылки",
+    id: 2,
+    items: [
+      {
+        name: "Button",
+        id: 3,
+      },
+      {
+        name: "Link",
+        id: 4,
+      }
+    ]
+  },
+  {
+    name: "Формы",
+    id: 3,
+    items: [
+      {
+        name: "Checkbox",
+        id: 5,
+      },
+      {
+        name: "CheckboxGroup",
+        id: 6,
+      },
+      {
+        name: "Input",
+        id: 7,
+      }
+    ]
+  },
+  {
+    name: "Карточки",
+    id: 4,
+    items: [
+      {
+        name: "Card",
+        id: 8,
+      },
+    ]
+  },
+];
+
+export default data;
